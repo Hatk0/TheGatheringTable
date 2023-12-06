4a48ac640671dc10ef8cb5736c29366fed2337c7
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-12-06 17:59:39 +0300

    Create a model for The Gathering API

--- TheGatheringTable/Sources/Scenes/Model/Model.swift
+++ TheGatheringTable/Sources/Scenes/Model/Model.swift
@@ -1,1 +1,22 @@
 import Foundation
+
+// MARK: - Card
+
+struct Card: Codable {
+    let cards: [CardElement]
+}
+
+// MARK: - CardElement
+
+struct CardElement: Codable {
+    let name: String
+    let type: String
+    let rarity: String
+    let text: String
+    let imageURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name, type, rarity, text
+        case imageURL = "imageUrl"
+    }
+}
