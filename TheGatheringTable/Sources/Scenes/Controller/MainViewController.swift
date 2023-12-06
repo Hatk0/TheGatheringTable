f628bcb250f89e298b18abde81e3a72eca03b1c1
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-12-06 16:38:56 +0300

    Prepare a project for further work

---
+++ TheGatheringTable/Sources/Scenes/Controller/MainViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    // MARK: - Configure
+    
+}
+
+// MARK: - Extensions
---
+++ TheGatheringTable/Sources/Scenes/View/MainView.swift
@@ -0,0 +1,34 @@
+import UIKit
+import SnapKit
+
+final class MainView: UIView {
+    
+    // MARK: - UI
+    
+    // MARK: - Initializers
+    
+    init() {
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupView() {
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+}
