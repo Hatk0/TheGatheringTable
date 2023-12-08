9c058e910f4bfae8113efdb18ce58a3be69a0052
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-12-08 17:35:40 +0300

    Add mainView, setup table and call fetchCard method

--- TheGatheringTable/Sources/Scenes/Controller/MainViewController.swift
+++ TheGatheringTable/Sources/Scenes/Controller/MainViewController.swift
@@ -1,14 +1,25 @@
 import UIKit
-import SnapKit
+import Alamofire
 
 class MainViewController: UIViewController {
+    
+    private var mainView: MainView?
+    
+    var card = [CardElement]()
 
     // MARK: - Lifecycle
+    
+    override func loadView() {
+        mainView = MainView()
+        view = mainView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupView()
+        configure()
+        fetchCard()
     }
     
     // MARK: - Setup
@@ -19,8 +30,52 @@ class MainViewController: UIViewController {
         view.backgroundColor = .systemBackground
     }
     
+    // MARK: - Fetch
+    
+    func fetchCard() {
+        CardManager.shared.fetchCard { [weak self] cards in
+                if let cards = cards {
+                    self?.card = cards
+                    self?.mainView?.mainTableView.reloadData()
+                } else {
+                    print("Failed to fetch cards")
+                }
+            }
+        }
+    
     // MARK: - Configure
     
+    private func configure() {
+        mainView?.mainTableView.register(MainCell.self, forCellReuseIdentifier: MainCell.identifier)
+        mainView?.mainTableView.dataSource = self
+        mainView?.mainTableView.delegate = self
+    }
 }
 
 // MARK: - Extensions
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        200
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return card.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MainCell.identifier, for: indexPath)
+                as? MainCell else {
+            return UITableViewCell()
+        }
+                
+        let currentCardElement = card[indexPath.row]
+                
+        cell.cardElement = currentCardElement
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
