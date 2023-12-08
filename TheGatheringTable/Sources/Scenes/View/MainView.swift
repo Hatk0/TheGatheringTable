b75ad777122303a75e503056388c192109912178
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-12-08 21:57:59 +0300

    Create searchBar and make constraints

--- TheGatheringTable/Sources/Scenes/View/MainView.swift
+++ TheGatheringTable/Sources/Scenes/View/MainView.swift
@@ -5,6 +5,14 @@ final class MainView: UIView {
     
     // MARK: - UI
     
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search"
+        searchBar.searchBarStyle = .minimal
+        searchBar.delegate = self
+        return searchBar
+    }()
+    
     lazy var mainTableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .plain)
         return tableView
@@ -30,12 +38,25 @@ final class MainView: UIView {
     }
     
     private func setupHierarchy() {
+        addSubview(searchBar)
         addSubview(mainTableView)
     }
     
     private func setupLayout() {
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(50)
+        }
+        
         mainTableView.snp.makeConstraints { make in
             make.top.left.bottom.right.equalToSuperview()
         }
     }
+}
+
+// MARK: - Extensions
+
+extension MainView: UISearchBarDelegate {
+    
 }
